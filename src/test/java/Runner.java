a42b4bc9ec1eeb4381b164a87ce4f1480a769f58
Randy <randyjgunawan1@gmail.com>
2024-08-05 21:29:32 +0700

    Add task 'cucumber' to build.gradle

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -3,11 +3,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        glue = {"stepDef"},
-        features = {"src/test/resources"},
-        plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json"}
-)
+//@CucumberOptions(
+//        glue = {"stepDef"},
+//        features = {"src/test/resources"},
+//        plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json"}
+//)
 
 public class Runner {
 }
